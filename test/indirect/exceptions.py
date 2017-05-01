221b8c7d00468f760ea9a9e531c3bb006110ee27
jeff <jeffreyr@securitydept.com>
2017-05-01 10:23:57 -0400

    clean up unneded stuff and simplify the docker image

--- test/indirect/exceptions.py
+++
@@ -1,2 +0,0 @@
-class DockerStackError(Exception):
-    pass
--- test/indirect/test_kibana.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from .fixtures import kibana  # noqa: F401
-
-
-def test_kibana_is_200_ok(kibana):  # noqa: F811
-    assert kibana.get().status_code == 200
